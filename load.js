14dc210bb8441f6f674d4f4093b95b839d1e4d58
jimmyolo <victor0801x@gmail.com>
2023-03-20 01:45:50 +0800

    handle weird csv list...

--- load.js
+++ load.js
@@ -4,10 +4,14 @@ const readline = require('node:readline')
 const fs = require('node:fs')
 const path = require('node:path')
 
-module.exports = () => {
+module.exports = (filename = 'ftx_all.csv') => {
+  const users = new Map()
+  const tokenSet = new Set()
+  const incomplete = new Map()
+
   return new Promise((resolve, reject) => {
-    const csvPath = path.join(__dirname, 'ftx_all.csv')
-    console.log('checking/loading csv: ', csvPath)
+    const csvPath = path.join(__dirname, filename)
+    console.log('loading csv file: ', csvPath)
     try {
       fs.lstatSync(csvPath)
     } catch (err) {
@@ -20,15 +24,14 @@ module.exports = () => {
       crlfDelay: Infinity,
     })
 
-    const users = new Map()
-    const tokenSet = new Set()
-
-    const parseAssets = (cid = '', assets = '') => {
+    const parseAssets = (cid = '', assets = '', isIncomplete = false) => {
       // if (['01613473', '06786371'].includes(cid)) {
       //   console.log({ cid, assets })
       // }
       assets = assets.trim().replace(/"|\s/g, '')
       const tokens = assets.split(',')
+
+      if (isIncomplete) incomplete.set(cid, assets)
 
       tokens.forEach(t => {
         const token = t.match(/(?<name>\w+)(\((?<nft>\w+)\))?\[(?<vol>[+-]?\d+(\.\d+)?)\]/)?.groups || {}
@@ -43,6 +46,29 @@ module.exports = () => {
       })
     }
 
+    const _isCID = (cid = '') => cid.length === 8 && +cid > 0
+
+    const _parseNextUser = l => {
+      const comma = l.indexOf(',')
+      let id = l.slice(0, comma)
+
+      if (_isCID(id)) return { id, assets: l.slice(comma + 1) }
+
+      // special case for some users...
+      // 03508433 BCHBULL[39800.0000000000000000],BSVBULL[16500000.0000000000000000],...
+      const space = l.indexOf(' ')
+      const isLeadingQuote = l.charAt(0) === '"'
+      id = l.slice(isLeadingQuote ? 1 : 0, space)
+      if (_isCID(id)) {
+        return {
+          id,
+          assets: `${isLeadingQuote ? '"' : ''}${l.slice(space + 1)}`
+        }
+      }
+
+      return {}
+    }
+
     const onLine = (() => {
       let cid = ''
       let unfinished = ''
@@ -52,15 +78,15 @@ module.exports = () => {
         if (!l) return
 
         if (cid && unfinished) {
-          // uncompleted assets... too long?
-          const comma = l.indexOf(',')
-          const nextId = l.slice(0, comma)
-          if (nextId.length === 8 && +nextId > 0) {
-            parseAssets(cid, unfinished)
+          // incomplete assets... too long?
+          // example:
+          // 00334864,"ATOM[0.0843200000000000],BCH[0.0000000055000000],BTC[0.0000000019256250],BULL[0.0000042137540000],COPE[0.1553700000000000],CREAM[0.0000000025000000],ETH[0.0000000026000000],FTT[1474.9691854382876185],HOOD[0.0000000100000000],HOOD_PRE[-
+          let { id, assets } = _parseNextUser(l)
+          if (_isCID(id)) {
+            parseAssets(cid, unfinished, true)
             unfinished = ''
 
-            cid = nextId
-            const assets = l.slice(comma + 1)
+            cid = id
             if (assets.charAt(0) === '"' && !assets.endsWith(']"')) {
               unfinished = assets
               return
@@ -83,16 +109,24 @@ module.exports = () => {
 
         if (l.startsWith('Customer')) return
 
-        const comma = l.indexOf(',')
-        const id = cid = l.slice(0, comma)
-        const assets = l.slice(comma + 1) || ''
+        // const comma = l.indexOf(',')
+        // const id = cid = l.slice(0, comma)
+        // const assets = l.slice(comma + 1) || ''
+        let { id, assets } = _parseNextUser(l)
+        cid = id
+        if (!assets) {
+          console.log('no assets', { id, assets, line: l })
+        }
 
+        // incomplete assets... too long?
+        // example:
+        // 00334864,"ATOM[0.0843200000000000],BCH[0.0000000055000000],BTC[0.0000000019256250],BULL[0.0000042137540000],COPE[0.1553700000000000],CREAM[0.0000000025000000],ETH[0.0000000026000000],FTT[1474.9691854382876185],HOOD[0.0000000100000000],HOOD_PRE[-
         if (assets.charAt(0) === '"' && !assets.endsWith(']"')) {
           unfinished = assets
           return
         }
 
-        parseAssets(id, assets)
+        parseAssets(cid, assets)
         cid = ''
         unfinished = ''
       }
@@ -105,7 +139,7 @@ module.exports = () => {
       //   users: users.size,
       //   tokenSet: tokenSet,
       // })
-      resolve({ users, tokenSet })
+      resolve({ users, tokenSet, incomplete })
     })
   })
 }
